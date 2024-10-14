54bd2a401edc56f3d8915aba7df6dfda4e8b0853
DimitryGameDev <yanindima1980@gmail.com>
2024-10-14 15:23:12 +0300

    UPDui

--- Assets/Scripts/UI/LevelGUI.cs
+++ Assets/Scripts/UI/LevelGUI.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
-    public class PausePanel : MonoBehaviour
+public class LevelGUI : MonoBehaviour
     {
-        [SerializeField] private GameObject m_Panel;
+        [FormerlySerializedAs("m_Panel")] [SerializeField] private GameObject m_PausePanel;
+        [SerializeField] private GameObject m_SettingsPanel;
         [SerializeField] private GameObject pauseButton;
         private int controlID;
         
         private void Start()
         {
             controlID = PlayerPrefs.GetInt("Control");
-            m_Panel.SetActive(false);
+            m_PausePanel.SetActive(false);
+            m_SettingsPanel.SetActive(false);
             Time.timeScale = 1;
             
             if (controlID == 0)
@@ -19,18 +22,24 @@ using UnityEngine.SceneManagement;
         
         public void ShowPause()
         {
-            m_Panel.SetActive(true);
+            m_SettingsPanel.SetActive(false);
+            m_PausePanel.SetActive(true);
             Time.timeScale = 0;
         }
-
+        public void EX_ShowSettingsPanel()
+        {
+            m_SettingsPanel.SetActive(true);
+            m_PausePanel.SetActive(false);
+            
+        }
         public void HidePause()
         {
-            m_Panel.SetActive(false);
+            m_PausePanel.SetActive(false);
             Time.timeScale = 1;
         }
         public void LoadMainMenu()
         {
-            m_Panel.SetActive(false);
+            m_PausePanel.SetActive(false);
             Time.timeScale = 1;
 
             SceneManager.LoadScene(0);
