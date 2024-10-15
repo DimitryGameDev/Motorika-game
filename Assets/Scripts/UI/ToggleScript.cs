4fa33075f4dd7e8dfd7dea4972f0da200ab0c8c7
DimitryGameDev <yanindima1980@gmail.com>
2024-10-16 02:41:17 +0300

    Add new UI texture assets
    Added five new UI texture files: Без заголовка.png, Волк.png, Дикошард.png, Жук1.png, and Жук2.png, along with their corresponding meta files. These assets will be used in various UI elements for the project. Each texture file has been configured with appropriate import settings for optimal performance.

--- Assets/Scripts/UI/ToggleScript.cs
+++ Assets/Scripts/UI/ToggleScript.cs
@@ -6,31 +6,58 @@ public class ToggleScript : MonoBehaviour
     [SerializeField] private string key;
     [SerializeField] private Sprite[] buttonSprites;
     [SerializeField] private Image targetButton;
-
+    [SerializeField] private Sprite[] otherbuttonBasicSprites;
+    [SerializeField] private Sprite[] otherbuttonChangedSprites;
+    [SerializeField] private Image[] otherButtons;
     private int ID;
 
     private void Start()
     {
         ID = PlayerPrefs.GetInt(key);
-
-        if (ID == 0)
-            targetButton.sprite = buttonSprites[0];
-        else
-            targetButton.sprite = buttonSprites[1];
+        if (key != "")
+        {
+            if (ID == 0)
+                targetButton.sprite = buttonSprites[0];
+            else
+                targetButton.sprite = buttonSprites[1];
+        }
+     
     }
 
     public void ChangeSprite()
     {
-        if (targetButton.sprite == buttonSprites[0])
+        if (key != "")
         {
-            targetButton.sprite = buttonSprites[1];
-            PlayerPrefs.SetInt(key, 1);
-            return;
+            if (targetButton.sprite == buttonSprites[0])
+            {
+                targetButton.sprite = buttonSprites[1];
+                PlayerPrefs.SetInt(key, 1);
+                return;
+            }
+            targetButton.sprite = buttonSprites[0];
+        
+            PlayerPrefs.SetInt(key, 0);
+            Debug.Log(PlayerPrefs.GetInt(key));
+            PlayerPrefs.Save();
         }
-        targetButton.sprite = buttonSprites[0];
+        else
+        {
+            if (targetButton.sprite == buttonSprites[0])
+            {
+                targetButton.sprite = buttonSprites[1];
+                return;
+            }
+            targetButton.sprite = buttonSprites[0];
 
-        PlayerPrefs.SetInt(key, 0);
-        Debug.Log(PlayerPrefs.GetInt(key));
-        PlayerPrefs.Save();
+        }
+    }
+
+
+    public void ChangeAllOtherSprites()
+    {
+     
+        for (var i = 0; i < otherButtons.Length; i++)
+            if (otherButtons[i].sprite == otherbuttonChangedSprites[i])
+                otherButtons[i].sprite = otherbuttonBasicSprites[i];
     }
 }
